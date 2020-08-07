86c76bedf7211d1c1cd5bdd86a168b6c6df0f0c2
bono566 <bono566@ad.liu.se>
2020-08-08 00:40:39 +0200

    DFS implemented but needs refactoring & cleanup.

--- src/pathfinding_visualizer/algorithms/implementations/DepthFirstSearch.java
+++ src/pathfinding_visualizer/algorithms/implementations/DepthFirstSearch.java
@@ -6,22 +6,53 @@ import pathfinding_visualizer.algorithms.configurations.TraversalStrategy;
 import pathfinding_visualizer.nodes.DefaultNode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class DepthFirstSearch implements Algorithm
 {
     private Board board;
+    private TraversalStrategy traversalStrategy;
     public DepthFirstSearch(Board board, TraversalStrategy traversalStrategy) {
         this.board = board;
+        this.traversalStrategy = traversalStrategy;
     }
 
-    @Override public ArrayList<DefaultNode> startSearch() {
-	return null;
+    public ArrayList<DefaultNode> startSearch() {
+        ArrayList<DefaultNode> visitedNodes = new ArrayList<>();
+        Stack<DefaultNode> openNodes = new Stack<>();
+        DefaultNode startNode = board.getStartNode();
+
+        openNodes.push(startNode);
+        while (!openNodes.isEmpty()) {
+            DefaultNode currentNode = openNodes.pop();
+            if (!currentNode.isStartNode() && !visitedNodes.contains(currentNode)) {
+                visitedNodes.add(currentNode);
+            }
+            if (currentNode.isEndNode()) {
+                return visitedNodes;
+            }
+            ArrayList<DefaultNode> neighbours = traversalStrategy.getNeighbours(board, currentNode);
+            for (DefaultNode neighbour: neighbours) {
+                if (!visitedNodes.contains(neighbour)) {
+                    openNodes.push(neighbour);
+                    neighbour.setParent(currentNode);
+                    if (!neighbour.isStartNode()) {
+                        visitedNodes.add(neighbour);
+                    }
+                    if (neighbour.isEndNode()) {
+                        return visitedNodes;
+                    }
+                }
+            }
+        }
+        return visitedNodes; // No path found.
+
     }
 
-    @Override public void setTraversalStrategy(final TraversalStrategy traversalStrategy) {
-
+    public void setTraversalStrategy(TraversalStrategy traversalStrategy) {
+        this.traversalStrategy = traversalStrategy;
     }
     public TraversalStrategy getTraversalStrategy() {
-        return null;
+        return traversalStrategy;
     }
 }
